2c89b1636a03dfcec16819149ef779612a7a5c9b
Sidheswar Ghosh <sidheswar.ghosh@gmail.com>
2019-04-16 03:45:53 +0200

    Fix notebook issue

--- blog_post.py
+++ blog_post.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# This solution is described about the Data Scientist Blog Post. CRISP-DM process will be applied.
+# ## This solution is described about the Data Scientist Blog Post. CRISP-DM process will be applied.
 
-# In[1]:
+# In[2]:
 
 
 # import libraries
@@ -15,22 +15,19 @@ from sklearn.metrics import r2_score, mean_squared_error, median_absolute_error
 from sklearn.ensemble import RandomForestRegressor
 
 
-# 
 # # 1. Business Understanding
-# 
-# It would be quite interesting to apply data analysis skills here as a football fan. I chose FIFA 19 complete player dataset.
-# I will fouc on mentioned below questions:
+
+# It would be quite interesting to apply data analysis skills here as a football fan. I chose FIFA 19 complete player dataset. I will fouc on mentioned below questions:
 # 
 # Q1. What is the ratio of total wages/ total potential for clubs. Which clubs are the most economical？
 # 
-# Q2. How is nation team player in total market value distributed? 
+# Q2. How is nation team player in total market value distributed?
 # 
 # Q3. Which player skils set influence potential/wage? Can we predict player/player's potential based on his skills set?
-# 
 
 # # 2. Data Understanding and Exploration
 
-# In[2]:
+# In[3]:
 
 
 # Load dataset
@@ -38,28 +35,28 @@ fifa19_player_data_frame = pd.read_csv('data.csv')
 fifa19_player_data_frame.head()
 
 
-# In[3]:
+# In[4]:
 
 
 # Number of players
 fifa19_player_data_frame.shape[0]
 
 
-# In[4]:
+# In[5]:
 
 
 # Data format for each column
 fifa19_player_data_frame.info()
 
 
-# In[5]:
+# In[6]:
 
 
 # Types of informations in data set
 fifa19_player_data_frame.columns
 
 
-# In[6]:
+# In[7]:
 
 
 # Missing values
@@ -67,17 +64,16 @@ fifa19_player_data_frame.isnull().sum()
 
 
 # # 3. Prepare Data
-# 
+
 # As per data exploration in above section, tthere are some necessary steps to be applied before preparing data:
 # 
-#     1. Unused column to be dropted
+# 1. Unused column to be dropted
 # 
-#     2. String to be converted to number
+# 2. String to be converted to number
 # 
-#     3. Handle missing values, if necessary drop them
-# 
+# 3. Handle missing values, if necessary drop them
 
-# In[7]:
+# In[8]:
 
 
 # Drop unused columns
@@ -90,14 +86,14 @@ columns_to_drop = ['Unnamed: 0', 'ID', 'Photo', 'Flag','Club Logo', 'Preferred F
 fifa19_player_data_frame.drop(columns_to_drop, axis=1, inplace=True)
 
 
-# In[8]:
+# In[9]:
 
 
 # Display data after dropped column
 fifa19_player_data_frame.head()
 
 
-# In[9]:
+# In[10]:
 
 
 # Convert value and wage columns string to number
@@ -120,7 +116,7 @@ def string2number(amount_str):
         return float(amount_str[1:])
 
 
-# In[10]:
+# In[11]:
 
 
 # First convert value, wage string to actual amount, then divide by 1 million and 1 thousand. 
@@ -132,50 +128,51 @@ fifa19_player_data_frame['Wage_K'] = fifa19_player_data_frame['Wage'].apply(lamb
 fifa19_player_data_frame.drop(['Value', 'Wage'], axis=1, inplace=True)
 
 
-# In[11]:
+# In[12]:
 
 
 # Display data set ater string to number conversion
 fifa19_player_data_frame.describe()
 
 
-# In[12]:
+# In[13]:
 
 
 # Find player Name who's Value is highest
 fifa19_player_data_frame.loc[fifa19_player_data_frame['Value_M'].idxmax()]
 
 
-# In[13]:
+# In[14]:
 
 
 # Find player Name who's Wage is highest
 fifa19_player_data_frame.loc[fifa19_player_data_frame['Wage_K'].idxmax()]
 
 
-# In[14]:
+# In[15]:
 
 
 # Missing value handling
 missing_player_data_frame = fifa19_player_data_frame[fifa19_player_data_frame['Agility'].isnull()]
 
 
-# In[15]:
+# In[16]:
 
 
 missing_player_data_frame.describe()
 
 
-# ### 3.1 Fifa19 data observation
+# ## 3.1 Fifa19 data observation
+
 # From above analysis result there are 48 missing values that quite a few columns which are related to player's skills.
 # 
 # So there were 48 players that simply missing those values. But, to answer Question-1 and Question-2 we will reserve those players since there were no missing value in Value_M and Wage_K column.
 # 
 # To explain Question-3, we will drop those player rows since there are many missing values.
-# 
 
 # # 4. Answer Questions base on dataset
-# #### Q1. What is the ratio of total wages/ total potential for clubs. Which clubs are the most economical？
+
+# ### Q1. What is the ratio of total wages/ total potential for clubs. Which clubs are the most economical？
 
 # In[17]:
 
@@ -189,7 +186,7 @@ club_wages = fifa19_player_data_frame.groupby('Club').sum()
 club_player_count = fifa19_player_data_frame.groupby('Club').count()
 
 
-# In[20]:
+# In[19]:
 
 
 # Total Number of clubs and average number of players in each club
@@ -199,7 +196,7 @@ print('Total Average wage(K) potential ratio is {}'
       .format(round(club_wages['Wage_K'].sum() / club_wages['Potential'].sum(), 2)))
 
 
-# In[21]:
+# In[20]:
 
 
 club_wages['Wage/Potential'] = club_wages['Wage_K'] / club_wages['Potential']
@@ -207,26 +204,26 @@ club_wages['Player Number'] = club_player_count['Age']
 club_wages['Player Average Age'] = club_wages['Age'] / club_wages['Player Number']
 
 
-# In[22]:
+# In[21]:
 
 
 club_wages.sort_values('Wage/Potential', ascending=False, inplace=True)
 
 
-# In[23]:
+# In[22]:
 
 
 club_wages.head()
 
 
-# In[36]:
+# In[23]:
 
 
 club_wages['Wage/Potential'].head(10).plot(kind='bar', color='Blue')
 plt.title('Top 10 clubs spending wage on players potential')
 
 
-# In[37]:
+# In[24]:
 
 
 club_wages['Wage/Potential'].tail(10).plot(kind='bar', color='Blue')
@@ -237,9 +234,9 @@ plt.title('Top 10 economical clubs ')
 # 
 # The economical clubs are not famous and from nowhere that we heard about. Few of them are quite famous like AEK Athens, Dynamo Kyiv may be more. This conclude that those club's players are potiential but underpayed. It would be good approach for 'Giant' clubs to bring more econimical players to reduce their overall wage spent.
 
-# #### Q2. How is nation team player in total market value distributed?
+# ### Q2. How is nation team player in total market value distributed?
 
-# In[39]:
+# In[25]:
 
 
 # Age count
@@ -247,14 +244,14 @@ age_count = fifa19_player_data_frame['Age'].value_counts()
 age_count.sort_index(ascending=True, inplace=True)
 
 
-# In[41]:
+# In[26]:
 
 
 # Calculate average overall rating
 age_mean = fifa19_player_data_frame.groupby('Age').mean()
 
 
-# In[43]:
+# In[27]:
 
 
 # Collect age distribuion and overall rating together
@@ -262,7 +259,7 @@ age_count_list = age_count.values.tolist()
 age_overall_rating_list = age_mean['Overall'].values.tolist()
 
 
-# In[47]:
+# In[30]:
 
 
 # Plot age distribution and overall rating together
@@ -274,7 +271,7 @@ axis_1.legend(loc=1)
 axis_1.set_ylabel('Average Rating')
 
 axis_2 = axis_1.twinx()
-plt.bar(ages, age_count_list, label='Age Count')
+plt.bar(age, age_count_list, label='Age Count')
 axis_2.legend(loc=2)
 axis_2.set_ylabel('Age Count')
 plt.show()
@@ -286,9 +283,9 @@ plt.show()
 # 
 # But there are also set of player's rating can remain quite high with age over 37, 38 years.
 
-# #### Q3. Which player skils set influence potential/wage? Can we predict player/player's potential based on his skills set?
+# ### Q3. Which player skils set influence potential/wage? Can we predict player/player's potential based on his skills set?
 
-# In[49]:
+# In[31]:
 
 
 # Drop unused columns to answers Question-3
@@ -296,14 +293,14 @@ columns_to_drop_q3 = ['Name', 'Nationality', 'Club']
 fifa19_player_data_frame.drop(columns_to_drop_q3, axis=1, inplace=True)
 
 
-# In[50]:
+# In[32]:
 
 
 # Drop players whose skill set is missing.
 fifa19_player_data_frame.dropna(axis=0, how='any', inplace=True)
 
 
-# In[51]:
+# In[33]:
 
 
 # Split Work Rate is in format of attack work rate and defence work rate
@@ -312,20 +309,20 @@ fifa19_player_data_frame['Work Rate Attack'] = fifa19_player_data_frame['Work Ra
 fifa19_player_data_frame['Work Rate Defence'] = fifa19_player_data_frame['Work Rate'].map(lambda x: x.split('/')[1])
 
 
-# In[52]:
+# In[34]:
 
 
 #Drop origin Work Rate column
 fifa19_player_data_frame.drop('Work Rate', axis=1, inplace=True)
 
 
-# In[53]:
+# In[35]:
 
 
 fifa19_player_data_frame.head()
 
 
-# In[54]:
+# In[36]:
 
 
 # One Hot Encoding for Position, Work Rate Attack and Work Rate Defence
@@ -333,7 +330,7 @@ one_hot_columns = ['Position', 'Work Rate Attack', 'Work Rate Defence']
 fifa19_player_data_frame = pd.get_dummies(fifa19_player_data_frame, columns=one_hot_columns, prefix = one_hot_columns)
 
 
-# In[55]:
+# In[37]:
 
 
 fifa19_player_data_frame.shape
@@ -341,20 +338,20 @@ fifa19_player_data_frame.shape
 
 # # 5. Train model and Performance Evaluation
 
-# In[56]:
+# In[38]:
 
 
 y = fifa19_player_data_frame['Potential']
 X = fifa19_player_data_frame.drop(['Value_M', 'Wage_K', 'Potential', 'Overall'], axis=1)
 
 
-# In[57]:
+# In[39]:
 
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.3, random_state=42)
 
 
-# In[58]:
+# In[40]:
 
 
 forest_regressor = RandomForestRegressor(n_estimators=500)
@@ -364,7 +361,7 @@ print(r2_score(y_test, y_test_preds))
 print(mean_squared_error(y_test, y_test_preds))
 
 
-# In[59]:
+# In[41]:
 
 
 coefs_df = pd.DataFrame()
@@ -378,7 +375,7 @@ coefs_df.sort_values('Coefs', ascending=False).head(10)
 # 
 # Players with excellent ball control and fast reactions tends to give us an outstanding performance in football match.
 
-# In[60]:
+# In[42]:
 
 
 coefs_df.set_index('Features', inplace=True)
